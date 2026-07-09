c6bdcb41a42ecdcdaa15cfe433f88dee2eb2751b
singharadhya97592-lgtm <singharadhya97592@gmail.com>
2026-07-09 16:38:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,2 @@
-const startBtn = document.getElementById("startBtn");
 
-startBtn.addEventListener("click", () => {
-
-    document.body.style.transition = "0.8s";
-    document.body.style.opacity = "0";
-
-    setTimeout(() => {
-
-        document.body.innerHTML = `
-        <div class="container">
-            <h1>🌿 Chapter One</h1>
-
-            <p class="quote">
-                "Every beautiful friendship starts with one hello..."
-            </p>
-
-            <button onclick="location.reload()">
-                💚 Go Back
-            </button>
-        </div>
-        `;
-
-        document.body.style.opacity = "1";
-
-    },800);
-
-});
+            
